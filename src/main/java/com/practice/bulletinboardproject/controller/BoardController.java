89d8f73eebf9d20a12c6501576e7d9c18175c11b
ryusun <ryusunshine@gmail.com>
2022-12-30 23:07:43 +0900

    enhancement
    글 수정, 파일 업로드 기능을 구현하였습니다.

--- src/main/java/com/practice/bulletinboardproject/controller/BoardController.java
+++ src/main/java/com/practice/bulletinboardproject/controller/BoardController.java
@@ -6,8 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class BoardController {
@@ -21,9 +20,12 @@ public class BoardController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board){
+    public String boardWritePro(Board board, Model model){ //model에 담겨서 html로 넘어감
         boardService.write(board);
-        return "";
+
+        model.addAttribute("message", "글 작성이 완료되었습니다.");
+        model.addAttribute("searchURL", "/board/list");
+        return "message";
     }
 
     @GetMapping("/board/list")
@@ -31,4 +33,36 @@ public class BoardController {
         model.addAttribute("list", boardService.boardList());
         return "boardlist";
     }
+
+    @GetMapping("/board/view")
+    public String boardView(Model model, Integer id){ // http://localhost:8080/board/view?id=1
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+
+//    @GetMapping("/board/delete")
+//        public String boardDelete(Integer id){
+//            boardService.boardDelete(id);
+//            return "redirect:/board/list";
+//    }
+
+    @GetMapping("/board/modify/{id}") // 수정페이지
+    public String boardModify(@PathVariable("id") Integer id, Model model){
+        model.addAttribute("board", boardService.boardView(id));
+        //상세페이지에 있는 내용이랑 수정할때 넘어가는 내용이 같기때문에
+
+        return "boardmodify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, Model model){
+        Board tempBoard = boardService.boardView(id); //기존의 글 검색
+        tempBoard.setTitle(board.getTitle());
+        tempBoard.setContent(board.getContent());
+        boardService.write(tempBoard);
+        model.addAttribute("message", "글 수정이 완료되었습니다.");
+        model.addAttribute("searchURL", "/board/list");
+        return "message";
+    }
+
 }
